872f32541a8aadf1ee4f6be33357db4cd35807b7
exit-only <nick.inhofe@pdq.com>
2023-03-08 19:08:09 -0800

    device find fix

--- api/app/controllers/profiles_controller.rb
+++ api/app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
 
   def index
     if params[:device_id].present?
-      @device = account.devices.find!(params[:device_id])
+      @device   = account.devices.find(params[:device_id])
       @profiles = @device.profiles
     else
       @profiles = account.profiles.order(name: :asc)
