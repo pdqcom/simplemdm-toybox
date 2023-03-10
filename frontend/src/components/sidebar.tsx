595e8e1bc9c08e4c6810433e450f7f218171e9a9
exit-only <nick.inhofe@pdq.com>
2023-03-09 16:43:44 -0800

    refactor sidebar

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,29 +1,34 @@
-import {IconProps, List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import {IconProps, Link, List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
 import {AppSettingsAlt, Ballot, Group} from "@mui/icons-material";
-import {ComponentType, ReactNode} from "react";
+import {ComponentType} from "react";
+
+import NextLink from "next/link"
 
 type SidebarItemProps = {
     text: String,
+    href: String,
     Icon: ComponentType<IconProps>
 }
 
-const SidebarItem = ({text, Icon}: SidebarItemProps) => {
-    return <ListItem disablePadding>
-        <ListItemButton>
-            <ListItemIcon>
-                <Icon/>
-            </ListItemIcon>
-            <ListItemText primary={text}/>
-        </ListItemButton>
+const SidebarItem = ({text, href, Icon}: SidebarItemProps) => {
+    return <ListItem disablePadding key={href}>
+        <NextLink href={href} passHref>
+            <Link underline="none" color="#000" component={ListItemButton}>
+                <ListItemIcon>
+                    <Icon/>
+                </ListItemIcon>
+                <ListItemText primary={text}/>
+            </Link>
+        </NextLink>
     </ListItem>
 }
 
 const Sidebar = () => {
     return <nav>
         <List>
-            <SidebarItem text="Devices" Icon={AppSettingsAlt}/>
-            <SidebarItem text="Profiles" Icon={Ballot}/>
-            <SidebarItem text="Users" Icon={Group}/>
+            <SidebarItem text="Devices" href="/devices" Icon={AppSettingsAlt}/>
+            <SidebarItem text="Profiles" href="/profiles" Icon={Ballot}/>
+            <SidebarItem text="Users" href="/users" Icon={Group}/>
         </List>
     </nav>
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,8 @@
-import {Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
-import {AppSettingsAlt, Group, Ballot} from "@mui/icons-material";
-import Sidebar from "@/components/sidebar";
+
 export default function Home() {
   return (
     <>
-      <main>
-          <Grid container spacing={2}>
-              <Grid item xs={3}>
-                <Sidebar />
-              </Grid>
-              <Grid item xs={9}>
-                  main
-              </Grid>
-          </Grid>
-      </main>
+        main
     </>
   )
 }
