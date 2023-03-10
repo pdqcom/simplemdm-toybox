f67efdbabb774c0177df2444244312f6c54dc45c
exit-only <nick.inhofe@pdq.com>
2023-03-09 18:12:43 -0800

    logic tweak

--- frontend/src/pages/devices.tsx
+++ frontend/src/pages/devices.tsx
@@ -1,7 +1,7 @@
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import {DataGrid, GridRowsProp, GridColDef} from '@mui/x-data-grid';
 import axios from "axios";
-import {useEffect, useState} from "react";
 import useSWR from 'swr'
+
 const columns: GridColDef[] = [
     {field: 'id', headerName: 'id', width: 150},
     {field: 'serialNumber', headerName: 'Serial Number', width: 300},
@@ -10,18 +10,19 @@ const columns: GridColDef[] = [
 
 const getDevices = () => axios.get('/api/devices.json')
 export default function Devices() {
-    const { data: response, error, isLoading } = useSWR('/api/devices', getDevices)
-
-    const rows =  response?.data?.data || []
-    let dataGrid = <DataGrid rows={rows} columns={columns} />
-    if (error) dataGrid = <div>failed to load</div>
-    if (isLoading) dataGrid = <div>loading...</div>
-
+    const {data: response, error, isLoading} = useSWR('/api/devices', getDevices)
+    let dataGrid
+    if (error) {
+        dataGrid = <div>failed to load</div>
+    } else {
+        const rows: GridRowsProp = response?.data?.data || []
+        dataGrid = <DataGrid rows={rows} columns={columns} loading={isLoading}/>
+    }
     return (
         <>
             <h1>Devices</h1>
-            <div style={{ height: 500, width: '100%' }}>
-                { dataGrid }
+            <div style={{height: 500, width: '100%'}}>
+                {dataGrid}
             </div>
 
         </>
