85809b37d1561b760fde1fd7187e9bbfd62c509e
exit-only <nick.inhofe@pdq.com>
2023-03-13 17:07:07 -0700

    theme and cleanup

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,12 @@
+import {createTheme} from "@mui/material";
+import {purple} from "@mui/material/colors";
+
+const theme = createTheme({
+    palette: {
+        background: {
+            default: "#f8fafd"
+        }
+    },
+})
+
+export default theme
