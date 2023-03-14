ef654f5920f4989f53471a41a8715b87208f924f
exit-only <nick.inhofe@pdq.com>
2023-03-13 17:07:43 -0700

    redirect to /devices

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,14 @@
 
+
+export const getServerSideProps = async ({ res }) => {
+    res.setHeader("Location", `/devices`);
+
+    res.statusCode = 302;
+    res.end();
+
+    return { props: {} };
+};
+
 const Home = () => {
 
   return (
